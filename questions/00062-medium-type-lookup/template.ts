6974ce48bf7a527cc11e890f6f8344752fd77163
xieshuang <930571051@qq.com>
2023-06-06 16:19:17 +0800

    feat: ts

--- questions/00062-medium-type-lookup/template.ts
+++ questions/00062-medium-type-lookup/template.ts
@@ -1,1 +1,9 @@
-type LookUp<U, T> = any
+/*
+ * @Author: xs
+ * @Date: 2023-06-05 13:50:23
+ */
+// type LookUp<U, T extends string> = {
+//    [K in keyof U]: U[K] extends T ? U[K] : never
+// }
+
+type LookUp<U, T extends string> = U extends { type: T } ? U : never
--- questions/00106-medium-trimleft/test-cases.ts
+++ questions/00106-medium-trimleft/test-cases.ts
@@ -1,3 +1,7 @@
+/*
+ * @Author: xs
+ * @Date: 2023-06-05 13:50:23
+ */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 type cases = [
