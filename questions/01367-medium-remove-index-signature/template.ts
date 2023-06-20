2de9749154e1862138dcd2b197a020495ebb2f14
xieshuang <930571051@qq.com>
2023-06-20 16:46:29 +0800

    feat: 01367

--- questions/01367-medium-remove-index-signature/template.ts
+++ questions/01367-medium-remove-index-signature/template.ts
@@ -2,4 +2,6 @@
  * @Author: xs
  * @Date: 2023-06-05 13:50:23
  */
-type RemoveIndexSignature<T> = { [k in keyof T as string extends k ? never : number extends k? never : symbol extends k ? never : k]: T[k] }
+type RemoveIndexSignature<T> = {
+   [k in keyof T as string extends k ? never : number extends k ? never : symbol extends k ? never : k]: T[k]
+}
